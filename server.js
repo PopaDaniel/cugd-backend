bc300740d6a18bf5e53e9b8c2e238fc8a84e6cc7
PopaDaniel <77845766+PopaDaniel@users.noreply.github.com>
2023-05-04 12:25:03 +0300

    update

--- server.js
+++ server.js
@@ -77,17 +77,6 @@ app.post("/addNewEmployee", upload.single("img"), (req, res) => {
   addEmployee.handleAddNewEmployee(req, res, Employee);
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
-
 app.listen(process.env.PORT, () => {
   console.log(`App listen on port ${process.env.PORT}`);
 });
