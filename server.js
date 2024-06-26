7f6db9fd2be345ff6bd7883267fe45a97635da3e
PopaDaniel <77845766+PopaDaniel@users.noreply.github.com>
2024-06-26 11:40:09 +0300

    last commit

--- server.js
+++ server.js
@@ -43,7 +43,9 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 initializeClient(app);
-
+app.get("/", (req, res) => {
+  res.json("Hello!");
+});
 // Import routes
 const connectRoutes = require("./routes/connectRoutes");
 const statusRoutes = require("./routes/statusRoutes");
