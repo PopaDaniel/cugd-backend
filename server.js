5fa968443018fa2b2e45f80cc49d2d6df31e40b0
PopaDaniel <77845766+PopaDaniel@users.noreply.github.com>
2024-06-27 00:15:22 +0300

    last commit

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get("/", (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-app.listen(process.env.PORT, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log(`App listening on port ${process.env.PORT}`);
 });
